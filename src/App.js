2d4faef044d170a7e1f5cd361f0caaedaebe7afb
timstarkk <timstarkk@icloud.com>
2020-03-22 16:09:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { API, graphqlOperation } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const query = `
+  query {
+    listTodos {
+      items {
+        id name description
+      }
+    }
+  }
+`
+
+export default class App extends Component {
+  state = {
+    todos: []
+  }
+
+  async componentDidMount() {
+    const data = await API.graphql(graphqlOperation(query));
+    this.setState({
+      todos: data.data.listTodos.items
+    })
+  }
+
+  handleButtonClick = async function () {
+    let name = document.getElementById('input1').value
+    let description = document.getElementById('input2').value
+
+    console.log(name);
+    console.log(description);
+    const addItem = `
+      mutation {
+        createTodo(input: {
+          name: "${name}"
+          description: "${description}"
+        }) {
+          id name description
+        }
+      }
+    `
+    API.graphql(graphqlOperation(addItem))
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.todos.map((todo, index) => (
+            <p key={index}>{todo.name}</p>
+          ))
+        }
+        <input type="text" id="input1" placeholder="name..." />
+        <input type="text" id="input2" placeholder="description..." />
+        <button id="theButton" onClick={() => { this.handleButtonClick() }}>add item</button>
+      </div >
+    )
+  }
 }
-
-export default App;
